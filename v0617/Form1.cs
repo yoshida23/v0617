6d4f9670acd53871daec7819efa5254b80795b5c
A601_03 <tnk.dat.class@gmail.com>
2021-06-17 11:38:33 +0900

    ラベル止めるマウス

--- v0617/Form1.cs
+++ v0617/Form1.cs
@@ -25,32 +25,41 @@ namespace v0617
         {
             Point spos = MousePosition;
             Point fpos = PointToClient(spos);
-            label3.Left=fpos. X-label3.Width/2;
-            label3.Top = fpos.Y-label3.Height/2;
+            label3.Left = fpos.X - label3.Width / 2;
+            label3.Top = fpos.Y - label3.Height / 2;
             label3.Text = $"{fpos.X},{fpos.Y}";
 
             label1.Left += vx;
             label1.Top += vy;
 
-            if(label1.Left<0)
+            if (label1.Left < 0)
             {
                 vx = Math.Abs(vx);
             }
-            if(label1.Top<0)
+            if (label1.Top < 0)
             {
                 vy = Math.Abs(vy);
             }
-            if(label1.Right>ClientSize.Width)
+            if (label1.Right > ClientSize.Width)
             {
                 vx = -Math.Abs(vx);
             }
-            if(label1.Bottom>ClientSize.Height)
+            if (label1.Bottom > ClientSize.Height)
             {
                 vy = -Math.Abs(vy);
             }
 
             score--;
             label2.Text = $"score{score}";
+
+            if ((fpos.X >= label1.Left)
+                && (fpos.X < label1.Right)
+                &&  (fpos.Y>=label1.Top)
+                &&  (fpos.Y<label1.Bottom)
+                )
+            {
+                timer1.Enabled = false;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
